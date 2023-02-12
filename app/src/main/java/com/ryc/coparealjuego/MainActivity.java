976537cda7d0d15e12099f1417d7d526218662d3
Elnuget <100396747+Elnuget@users.noreply.github.com>
2023-02-12 18:46:43 -0500

    fondo y cambio intento 1

--- app/src/main/java/com/ryc/coparealjuego/MainActivity.java
+++ app/src/main/java/com/ryc/coparealjuego/MainActivity.java
@@ -1,124 +1,32 @@
 package com.ryc.coparealjuego;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.VideoView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    VideoView vv_fondo;
-
-    MediaPlayer mMediaPlayer;
-
-    int mCurrentVideoPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vv_fondo = findViewById(R.id.vv_fondo);
 
-
-
-        Uri uri = Uri.parse("android.resource://"
-
-                + getPackageName()
-
-                + "/"
-
-                + R.raw.paisaje_fondo
-
-        );
-
-
-
-        vv_fondo.setVideoURI(uri);
-
-
-
-        vv_fondo.start();
-
-
-
-
-
-        vv_fondo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-
+        View mainView = findViewById(R.id.MainActivity);
+        mainView.setOnClickListener(new View.OnClickListener() {
             @Override
-
-            public void onPrepared(MediaPlayer mediaPlayer) {
-
-                mMediaPlayer = mediaPlayer;
-
-
-
-                mMediaPlayer.setLooping(true);
-
-
-
-                if(mCurrentVideoPosition != 0){
-
-                    mMediaPlayer.seekTo(mCurrentVideoPosition);
-
-                    mMediaPlayer.start();
-
-                }
-
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PrincipalActivity.class);
+                startActivity(intent);
             }
-
         });
 
-
-
     }
 
-
-
-
-
-    @Override
-
-    protected void onPause() {
-
-        super.onPause();
-
-
-
-        mCurrentVideoPosition = mMediaPlayer.getCurrentPosition();
-
-        vv_fondo.pause();
-
-    }
-
-
-
-    @Override
-
-    protected void onResume() {
-
-        super.onResume();
-
-
-
-        vv_fondo.start();
-
-    }
-
-
-
-    @Override
-
-    protected void onDestroy() {
-
-        super.onDestroy();
-
-
-
-        mMediaPlayer.release();
-
-        mMediaPlayer = null;
-
-    }
 }
+
+
+
+
+
+
