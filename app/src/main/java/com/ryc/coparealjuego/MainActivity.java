9d2ad83b5f9eff1b774573d2e21e35c9378556b5
Elnuget <100396747+Elnuget@users.noreply.github.com>
2023-02-20 19:39:22 -0500

    Animaciones de Pagina inciial

--- app/src/main/java/com/ryc/coparealjuego/MainActivity.java
+++ app/src/main/java/com/ryc/coparealjuego/MainActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation anim1 = AnimationUtils.loadAnimation(this, R.anim.blink);
+        TextView textView3 = findViewById(R.id.textView3);
+        textView3.setVisibility(View.VISIBLE);
+        textView3.startAnimation(anim1);
+
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.scale);
+        ImageView imageView2 = findViewById(R.id.imageView2);
+        imageView2.startAnimation(anim);
+
 
         View mainView = findViewById(R.id.MainActivity);
         mainView.setOnClickListener(new View.OnClickListener() {
