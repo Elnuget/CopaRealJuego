d4cd10c6f84a3dc080d3ddf30773657a93c82a13
Administrator <cangulo009@outlook.es>
2023-03-22 15:12:17 -0700

    BotonesFuncionando, correccion de imagenes

---
+++ app/src/main/java/com/ryc/coparealjuego/NuevoActivity.java
@@ -0,0 +1,26 @@
+package com.ryc.coparealjuego;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class NuevoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nuevojuego);
+
+        Animation anim1 = AnimationUtils.loadAnimation(this, R.anim.blink);
+        TextView textView3 = findViewById(R.id.textView3);
+        textView3.setVisibility(View.VISIBLE);
+        textView3.startAnimation(anim1);
+
+    }
+}
--- app/src/main/java/com/ryc/coparealjuego/PrincipalActivity.java
+++ app/src/main/java/com/ryc/coparealjuego/PrincipalActivity.java
@@ -1,12 +1,50 @@
 package com.ryc.coparealjuego;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class PrincipalActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.scale);
+        ImageView imageView2 = findViewById(R.id.imageView30);
+        imageView2.startAnimation(anim);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrincipalActivity.this, NuevoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView8 = findViewById(R.id.imageView8);
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrincipalActivity.this, LogrosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imageView9 = findViewById(R.id.imageView9);
+        imageView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrincipalActivity.this, ConfActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
